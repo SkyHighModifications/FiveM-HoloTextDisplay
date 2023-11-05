c70aba34969b5ab5def533b9f785cee17d24e1a7
SkyHigh Modifications <93752549+SkyHighModifications@users.noreply.github.com>
2023-11-05 07:20:48 +0000

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,8 +1,11 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
 
-author 'Berkshire Modifications'
-version '1.0'
+description 'Text Hologram Script for FiveM'
+author 'SkyHigh Modifications'
+version '1.0.0'
 
-client_script 'holograms.lua'
+client_scripts {
+    'holograms.lua'
+}
 
-
+server_script "version.lua"
